35b8c9a9dc10964833620f98de1920409bca29fe
debas-31 <debas31@gmail.com>
2022-07-07 17:39:10 +0300

    Add validation for post and user classes of models

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,10 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(5)
   end
 
+  validate :title , presence: true, allow_blank: false, length: { maximum: 250 }
+  validate :comments_counter, numericality: { only_integer: true, greater_than: -1 }
+  validate :likes_counter, numericality: { only_integer: true, greater_than: -1 }
+
   private
 
   def update_users_posts_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
 
+  validate :name, presence: true, allow_blank: false
+  validate :posts_conunter, numericality: { only_integer: true, greater_than: -1 }
+
   def last_3_posts
     posts.order(created_at: :desc).limit(3)
   end
