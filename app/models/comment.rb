f3e0c23d8303b23cd5615e81f664f9924acec00a
debas-31 <debas31@gmail.com>
2022-07-07 14:48:46 +0300

    Apply rails active callbacks  recommended by the reviewer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,12 @@
 class Comment < ApplicationRecord
+  after_save :update_posts_comments_counter
+
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
+  private
+
   def update_posts_comments_conuter
     post.increment!(:comments_counter)
-    post.save
   end
 end
