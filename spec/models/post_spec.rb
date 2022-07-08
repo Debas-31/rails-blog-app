70af60e738cae896593af3454a74f633b7aacf1c
debas-31 <debas31@gmail.com>
2022-07-08 18:48:35 +0300

    Add testing to post class

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'Validations For the Post model' do
+    before(:each) do
+      @user = User.new(id: 1, name: 'Debas', photo: 'image_link', bio: 'full-stack Developer')
+      @post = Post.new(author: @user, title: 'post 1', text: 'post content', comments_counter: 0, likes_counter: 0)
+      @like = Like.new(author_id: @user.id, post_id: @post.id)
+    end
+
+    it 'if all atributes pass validation' do
+      expect(@post).to be_valid
+    end
+
+    it 'if there is title' do
+      @post.title = nil
+      expect(@post).not_to be_valid
+    end
+
+    it 'if there is max 250 characters' do
+      @post.title = 'Testing lenght'
+      expect(@post).to be_valid
+    end
+
+    it 'if likes counter is integer' do
+      @post.likes_counter = 'string'
+      expect(@post).not_to be_valid
+    end
+
+    it 'if likes counter greater than or equal to zero' do
+      @post.likes_counter = -25
+      expect(@post).not_to be_valid
+    end
+
+    it 'if comments counter greater than or equal to zero.' do
+      @post.comments_counter = -2
+      expect(@post).not_to be_valid
+    end
+
+    it 'if comments counter is integer' do
+      @post.comments_counter = 0.25
+      expect(@post).not_to be_valid
+    end
+
+    it 'if comments counter is integer' do
+      @post.comments_counter = 15
+      expect(@post).to be_valid
+    end
+  end
+end
