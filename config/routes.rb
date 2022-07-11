e73b2dd36b56545f4b10286d33f156adabd5ce24
debas-31 <debas31@gmail.com>
2022-07-11 17:28:44 +0300

    Short hand way of representing a code recommended

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+=begin
+    get '/users/:user_id/posts', to: 'posts#index'
+    get '/users/:user_id/posts/:id', to: 'posts#show'
+    get '/users', to: 'users#index'
+    get '/users/:id', to: 'users#show'
 
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
+    Equivalent to the one below
+
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show]
+    end
+=end
+  
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+
   # Defines the root path route ("/")
   root "users#index"
 end
