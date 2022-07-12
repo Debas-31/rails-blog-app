1685043cc6f3229835fab032916bfe8eca5a98c7
debas-31 <debas31@gmail.com>
2022-07-12 18:02:40 +0300

    Add tests for users controller of index method

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:each) do
+      get '/users'
+    end
+
+    it 'GET request response status was correct' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders a correct template' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'response body includes correct placeholder text' do
+      expect(response.body).to include('Here is a list of users page')
+    end
+
+    
   end
 end
