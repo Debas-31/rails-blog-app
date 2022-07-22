639f2b93a2ae98362bfeff457da2195f2ffba4e8
debas-31 <debas31@gmail.com>
2022-07-22 12:19:07 +0300

    Fix authentication in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_request
 
   protected
 
@@ -11,10 +12,10 @@ class ApplicationController < ActionController::Base
       u.permit(:name, :photo, :bio, :email, :password, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+      u.permit(:name, :photo, :bio, :email, :password, :password_confirmation, :current_password)
     end
   end
-  
+
   def authenticate_request
     header = request.headers['Authorization']
     header = header.split.last if header
